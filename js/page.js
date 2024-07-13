6c4b00d0746a121a3563d29a1d921ca7e6666b43
AngelGlezz <angelglezz.7@gmail.com>
2024-07-12 20:45:31 -0600

    Initial commit

---
+++ js/page.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true
+    });
+});
